05c337ab62523ed774cafa7542f8e6d0e66772b8
fmartinezneumont <47900208+fmartinezneumont@users.noreply.github.com>
2019-02-26 09:49:00 -0700

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,28 +1,10 @@
 public class Main {
 
     public static void main(String[] args) {
-
-
-
         System.out.println("Hello world :)");
-
-        System.out.println("another tffhfgh     est");
-
-
-
-
-
+        System.out.println("another test");
         System.out.println("added something");
         System.out.println("This is a push test");
-
-
-
-
-
-
-
-
-        
     }
 
 }
