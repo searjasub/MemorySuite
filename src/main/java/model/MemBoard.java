fe3112247a74b593d9c41efd4ee6ae99f7afb6b0
fmartinezneumont <47900208+fmartinezneumont@users.noreply.github.com>
2019-02-27 11:16:14 -0700

    created memboard

--- src/main/java/model/MemBoard.java
+++ src/main/java/model/MemBoard.java
@@ -1,5 +1,31 @@
-package main.java.model;
+package model;
 
 public class MemBoard {
+    private int width;
+    private int height;
+    private MemBoardSquares[][] boardSquares = new MemBoardSquares[width][height];
 
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public MemBoardSquares[][] getBoardSquares() {
+        return boardSquares;
+    }
+
+    public void setBoardSquares(MemBoardSquares[][] boardSquares) {
+        this.boardSquares = boardSquares;
+    }
 }
---
+++ src/main/java/model/MemBoardSquares.java
@@ -0,0 +1,26 @@
+package model;
+
+/**
+ * The type Mem board squares.
+ */
+public class MemBoardSquares {
+    private CardTypes type;
+
+    /**
+     * Gets type.
+     *
+     * @return the type
+     */
+    public CardTypes getType() {
+        return type;
+    }
+
+    /**
+     * Sets type.
+     *
+     * @param type the type
+     */
+    public void setType(CardTypes type) {
+        this.type = type;
+    }
+}
