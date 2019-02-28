98ea6a6b8601b2cc9723027a84968a799d9d1a98
fmartinezneumont <47900208+fmartinezneumont@users.noreply.github.com>
2019-02-27 23:24:45 -0700

    Sequence and Number board lists created with getters and setters

--- src/main/java/edu/neumont/csc150/model/NumberBoard.java
+++ src/main/java/edu/neumont/csc150/model/NumberBoard.java
@@ -1,4 +1,29 @@
 package edu.neumont.csc150.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * The type Number board.
+ */
 public class NumberBoard {
+    private List<CardTypes> numbers = new ArrayList<>();
+
+    /**
+     * Gets numbers.
+     *
+     * @return the numbers
+     */
+    public List<CardTypes> getNumbers() {
+        return numbers;
+    }
+
+    /**
+     * Sets numbers.
+     *
+     * @param numbers the numbers
+     */
+    public void setNumbers(List<CardTypes> numbers) {
+        this.numbers = numbers;
+    }
 }
--- src/main/java/edu/neumont/csc150/model/SequenceBoard.java
+++ src/main/java/edu/neumont/csc150/model/SequenceBoard.java
@@ -1,4 +1,29 @@
 package edu.neumont.csc150.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * The type Sequence board.
+ */
 public class SequenceBoard {
+    private List<CardTypes> sequence = new ArrayList<>();
+
+    /**
+     * Gets sequence.
+     *
+     * @return the sequence
+     */
+    public List<CardTypes> getSequence() {
+        return sequence;
+    }
+
+    /**
+     * Sets sequence.
+     *
+     * @param sequence the sequence
+     */
+    public void setSequence(List<CardTypes> sequence) {
+        this.sequence = sequence;
+    }
 }
