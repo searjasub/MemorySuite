5e161d66a2a75934dfddd2300a4b547c46dcf947
fmartinezneumont <47900208+fmartinezneumont@users.noreply.github.com>
2019-03-04 12:19:49 -0700

    fixed sequence controller

--- src/main/java/edu/neumont/csc150/controller/SequenceGameController.java
+++ src/main/java/edu/neumont/csc150/controller/SequenceGameController.java
@@ -5,9 +5,11 @@ import edu.neumont.csc150.model.SequenceBoard;
 public class SequenceGameController {
   private SequenceBoard board = new SequenceBoard();
     private int cardAmount = 5;
+    private int wrongCards = 2;
 
     public void init(){
         generateCards();
+        generateChoices();
     }
 
     private void generateCards(){
@@ -16,5 +18,14 @@ public class SequenceGameController {
             this.board.getSequence().add(this.board.getCard((int)Math.random()*this.board.getCards().length));
         }
         this.cardAmount++;
+    }
+
+    private void generateChoices(){
+        this.board.getSequenceChoices().clear();
+        this.board.setSequence(this.board.getSequence());
+        for (int i = 0; i <wrongCards; i++) {
+            this.board.getSequenceChoices().add(this.board.getCard((int)Math.random()*this.board.getCards().length));
+        }
+        this.wrongCards++;
     }
 }
