0933c7e970e2a6611c136264afca367b271e869c
Jeffrey Rehm <47900191+j-rehm@users.noreply.github.com>
2019-03-07 02:51:08 -0700

    added matched boolean to MemBoardSquare

--- src/main/java/edu/neumont/csc150/model/MemBoardSquare.java
+++ src/main/java/edu/neumont/csc150/model/MemBoardSquare.java
@@ -6,11 +6,21 @@ public class MemBoardSquare extends Label {
 
     private CardType type;
 
+    private boolean matched = false;
+
     public CardType getType() {
         return type;
     }
 
     public void setType(CardType type) {
         this.type = type;
+    }
+
+    public boolean isMatched() {
+        return matched;
+    }
+
+    public void setMatched(boolean matched) {
+        this.matched = matched;
     }
 }
